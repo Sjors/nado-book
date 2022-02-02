61b66c64af624217325e312355b57898ec8c034e
Sjors Provoost <sjors@sprovoost.nl>
2022-02-02 12:15:05 +0100

    Option to skip mini qr codes during build

--- make_book.sh
+++ make_book.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+SKIP_QR="0"
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -p|--pdf) EXTRA_OPTIONS="-o nado-book.pdf --metadata-file meta-ebook.yaml"; shift ;;
         -e|--epub) EXTRA_OPTIONS="-t epub3 -o nado-book.epub --css epub.css --metadata-file meta-ebook.yaml --epub-chapter-level 2" ;;
         -b|--paperback) EXTRA_OPTIONS="-o nado-paperback.pdf --metadata-file meta-paperback.yaml --include-before-body copyright_paperback.tex" ;;
+        -q|--skipqr) SKIP_QR="1"; shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
@@ -14,39 +16,43 @@ find **/*processed* -exec rm -rf {} \;
 find intro.md **/*.md -exec cp {} {}.processed \;
 find . -name "*.md.processed" -exec sh -c 'mv "$1" "${1%.md.processed}.processed.md"' _ {} \;
 
-# Process episode QR codes:
-pushd qr/ep
-    for f in *.txt; do
-        qrencode -o ${f%.txt}.png -r $f --level=M -d 300 -s 6
-    done
-popd
+if [ "$SKIP_QR" -eq "0" ]; then
 
-# Process footnote QR codes:
-# Collect URL's:
-find intro.md **/*.md -print0 | xargs -0 perl -ne 'print "$1\n" while /<(http.*?)>/g' | sort | uniq > qr/note/urls.txt
-if ! git diff --quiet qr/note/urls.txt; then
-    echo "Please update bit.ly links for URLs:"
-    git diff qr/note/urls.txt
-    exit 1
-fi
+    # Process episode QR codes:
+    pushd qr/ep
+        for f in *.txt; do
+            qrencode -o ${f%.txt}.png -r $f --level=M -d 300 -s 6
+        done
+    popd
 
-if ! [ "$(wc -l < qr/note/urls.txt)" -eq "$(wc -l < qr/note/shorts.txt)" ]; then
-    echo "shorts.txt should have the same number of entries as urls.txt"
-    exit 1
-fi
-
-count=`wc -l < qr/note/urls.txt`
-for i in $(seq $count); do
-    url=`sed -n ${i}p qr/note/urls.txt`
-    short_url=`sed -n ${i}p qr/note/shorts.txt`
-    # Skip URL's that haven't been shortened
-    # Some domains are so short, they don't need shortening (unless we deep link to them)
-    if echo $short_url | grep 'bit.ly\|nus.edu\|amzn.to\|gitian.org'; then
-        echo $short_url | qrencode -o qr/note/${i}.png --level=L -8 -d 300 -s 2 --margin=1
-        # Add to processed markdown (might be macOS specific):
-        find intro.processed.md **/*.processed.md -exec sed -i '' -e "s#<$url>#<$url> ![](qr/note/$i.png)#g" {} \;
+    # Process footnote QR codes:
+    # Collect URL's:
+    find intro.md **/*.md -print0 | xargs -0 perl -ne 'print "$1\n" while /<(http.*?)>/g' | sort | uniq > qr/note/urls.txt
+    if ! git diff --quiet qr/note/urls.txt; then
+        echo "Please update bit.ly links for URLs:"
+        git diff qr/note/urls.txt
+        exit 1
     fi
-done
+
+    if ! [ "$(wc -l < qr/note/urls.txt)" -eq "$(wc -l < qr/note/shorts.txt)" ]; then
+        echo "shorts.txt should have the same number of entries as urls.txt"
+        exit 1
+    fi
+
+    count=`wc -l < qr/note/urls.txt`
+    for i in $(seq $count); do
+        url=`sed -n ${i}p qr/note/urls.txt`
+        short_url=`sed -n ${i}p qr/note/shorts.txt`
+        # Skip URL's that haven't been shortened
+        # Some domains are so short, they don't need shortening (unless we deep link to them)
+        if echo $short_url | grep 'bit.ly\|nus.edu\|amzn.to\|gitian.org'; then
+            echo $short_url | qrencode -o qr/note/${i}.png --level=L -8 -d 300 -s 2 --margin=1
+            # Add to processed markdown (might be macOS specific):
+            find intro.processed.md **/*.processed.md -exec sed -i '' -e "s#<$url>#<$url> ![](qr/note/$i.png)#g" {} \;
+        fi
+    done
+
+fi
 
 # Process figures:
 dot -Tsvg taproot/speedy_trial.dot > taproot/speedy_trial.svg
