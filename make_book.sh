20bfa358e4f293435b821eaa4206aae0230eac25
Sjors Provoost <sjors@sprovoost.nl>
2022-05-17 14:58:44 +0200

    Add \MiniQR helper

--- make_book.sh
+++ make_book.sh
@@ -54,12 +54,7 @@ if [ "$SKIP_QR" -eq "0" ]; then
     for i in $(seq $count); do
         url=`sed -n ${i}p qr/note/urls.csv`
         short_url=`sed -n ${i}p qr/note/shorts.txt`
-        # echo "s*<$url>*<$url> \\\qrcode[height=0.45cm,level=L]{$short_url}*g;" >> qr/sed
-        # width and height are 0.45cm in inches
-        # version is hardcoded to 1. For any QR code where this doesn't fiat,
-        # it will throw "GPL Ghostscript: Unrecoverable error"
-        # pspicture ensures any text following the QR is moved aside
-        echo "s*<$url>*<$url> \\\raisebox{-1.5mm}{\\\begin{pspicture}(0.45cm,0.45cm)\\\psbarcode{$short_url}{version=1 eclevel=L width=0.177 height=0.177}{qrcode}\\\end{pspicture}}*g;" >> qr/sed
+        echo "s*<$url>*<$url> \\\MiniQR{$short_url}*g;" >> qr/sed
     done
     find **/*.processed.md -exec sed -i '' -f qr/sed {} \;
 
